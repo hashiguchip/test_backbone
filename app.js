29bc3a8a6f8aa21c5f91391a3705f61f2cc86de0
hashiryo <hashiguchip@gmail.com>
2019-10-23 00:42:37 +0900

    feat: hello view

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+import jquery from "jquery";
+import * as Backbone from "backbone";
+
+/**
+ * Model
+ */
 console.log("hello");
+
+const TodoModel = Backbone.Model.extend({
+  constructor: function() {
+    console.log("TodoModelのコンストラクタメソッド");
+  }
+});
+
+const todo = new TodoModel({ a: "初期値" });
+// console.log(todo.get("a"));
+
+/**
+ * View
+ */
+const MyView = Backbone.View.extend({
+  tagName: "span",
+  className: "foo",
+  initialize: function(options) {
+    console.log("viewのinitialize");
+    this.render();
+  },
+  render: function() {
+    // 適当にレンダリングしてみる
+    this.$el.html("foooo");
+  }
+});
+
+const myView = new MyView({ el: "#test" });
