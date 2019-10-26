ae9de6a0d69610f4b97bce6fbc235750d264e8c1
hashiryo <hashiguchip@gmail.com>
2019-10-26 23:35:54 +0900

    feat: add collections

--- app.js
+++ app.js
@@ -21,9 +21,27 @@ const TodoModel = Backbone.Model.extend({
 const todo = new TodoModel({
   title: "タスクテスト！！！"
 });
-console.log(todo.get("title"));
 
 // console.log(todo.get("a"));
+
+/**
+ * Collection
+ */
+const TodoCollection = Backbone.Collection.extend({
+  model: TodoModel
+});
+
+const todos = new TodoCollection([
+  { title: "ごはん食べる", id: 1 },
+  { title: "ごみすてる", id: 2 }
+]);
+
+console.log(todos.get(1));
+
+todos.add(new TodoModel({ title: "引数で追加", id: 3 }));
+
+console.log("こうしｎ！！");
+console.log(todos);
 
 /**
  * View
@@ -32,21 +50,49 @@ const MyView = Backbone.View.extend({
   tagName: "span",
   className: "foo",
   model: todo,
+  collection: todos,
   template: _.template($("#test-template").html()),
   events: {
     click: "change"
   },
 
   initialize: function(options) {
+    this.listenTo(this.collection, "add", this.change);
     this.render();
   },
   render: function() {
-    this.$el.html(this.template(this.model.attributes));
+    //
+    const todoRecord = this.collection.models.map(model => {
+      return this.template(model.attributes);
+    });
+    this.$el.html(todoRecord.join(""));
   },
   change: function() {
-    todo.set({ title: "タイトル変更" });
+    // とりあえず更新するだけ
+    this.render();
+  }
+});
+
+const FormView = Backbone.View.extend({
+  className: "form",
+  template: _.template($("#submit_template").html()),
+  events: {
+    "click .task-input__submit": "submit"
+  },
+  collection: todos,
+
+  initialize: function(options) {
+    this.render();
+  },
+  render: function() {
+    this.$el.html(this.template());
+  },
+  submit: function() {
+    const newTaskTitle = $("#task-input").val();
+    this.collection.add({ title: newTaskTitle });
     this.render();
   }
 });
 
 const myView = new MyView({ el: "#test" });
+const formView = new FormView({ el: "#submit" });
